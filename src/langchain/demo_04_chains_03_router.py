b2dae5d15708bce2d135b9491967d2ff0412d082
Rohit Gargate <rohit24@meta.com>
2024-12-24 15:33:00 -0800

    updatges

---
+++ src/langchain/demo_04_chains_03_router.py
@@ -0,0 +1,126 @@
+from operator import itemgetter
+from typing import Literal
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableConfig
+from langgraph.graph import END, START, StateGraph
+from typing_extensions import TypedDict
+import asyncio
+
+llm = ChatOllama(model="llama3.2", temperature=0.9)
+
+# Define the prompts we will route to
+prompt_1 = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an expert on animals."),
+        ("human", "{input}"),
+    ]
+)
+prompt_2 = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an expert on vegetables."),
+        ("human", "{input}"),
+    ]
+)
+
+# Construct the chains we will route to. These format the input query
+# into the respective prompt, run it through a chat model, and cast
+# the result to a string.
+chain_1 = prompt_1 | llm | StrOutputParser()
+chain_2 = prompt_2 | llm | StrOutputParser()
+
+
+# Next: define the chain that selects which branch to route to.
+# Here we will take advantage of tool-calling features to force
+# the output to select one of two desired branches.
+route_system = "Route the user's query to either the animal or vegetable expert."
+route_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", route_system),
+        ("human", "{input}"),
+    ]
+)
+
+
+# Define schema for output:
+class RouteQuery(TypedDict):
+    """Route query to destination expert."""
+
+    destination: Literal["animal", "vegetable"]
+
+
+route_chain = route_prompt | llm.with_structured_output(RouteQuery)
+
+
+# For LangGraph, we will define the state of the graph to hold the query,
+# destination, and final answer.
+class State(TypedDict):
+    query: str
+    destination: RouteQuery
+    answer: str
+
+
+# We define functions for each node, including routing the query:
+async def route_query(state: State, config: RunnableConfig):
+    destination = await route_chain.ainvoke(state["query"], config)
+    return {"destination": destination}
+
+
+# And one node for each prompt
+async def prompt_1(state: State, config: RunnableConfig):
+    return {"answer": await chain_1.ainvoke(state["query"], config)}
+
+
+async def prompt_2(state: State, config: RunnableConfig):
+    return {"answer": await chain_2.ainvoke(state["query"], config)}
+
+
+# We then define logic that selects the prompt based on the classification
+def select_node(state: State) -> Literal["prompt_1", "prompt_2"]:
+    if state["destination"] == "animal":
+        return "prompt_1"
+    else:
+        return "prompt_2"
+
+
+# Finally, assemble the multi-prompt chain. This is a sequence of two steps:
+# 1) Select "animal" or "vegetable" via the route_chain, and collect the answer
+# alongside the input query.
+# 2) Route the input query to chain_1 or chain_2, based on the
+# selection.
+graph_builder = StateGraph(State)
+graph_builder.add_node("route_query", route_query)
+graph_builder.add_node("prompt_1", prompt_1)
+graph_builder.add_node("prompt_2", prompt_2)
+
+graph_builder.add_edge(START, "route_query")
+graph_builder.add_conditional_edges("route_query", select_node)
+graph_builder.add_edge("prompt_1", END)
+graph_builder.add_edge("prompt_2", END)
+graph = graph_builder.compile()
+
+
+async def run_chat(graph, config):
+    print("\nType 'exit | quit' to quit the application.\n")
+    while True:
+        # Prompt the user for input
+        user_input = input("User: ")
+
+        # Check if user wants to exit
+        user_input_lower = user_input.lower().strip()
+        if user_input_lower in ["exit", "quit"]:
+            print("Exiting...")
+            break
+
+        # Invoke the graph asynchronously
+        output = await graph.ainvoke(
+            {"query": [("user", user_input)]}, config, stream_mode="values"
+        )
+
+        # Print the system response
+        print(f"System: {output}\n---")
+
+
+config = {"configurable": {"thread_id": "pqr789"}}
+asyncio.run(run_chat(graph, config))
